3322807633dbcb531a94d8454ee127e9aeecf829
jiho <jihopark09@naver.com>
2025-08-03 19:40:48 +0900

    fix: 뉴스api params 변경 및 불필요 주석 삭제

--- src/fetch/news_fetcher.py
+++ src/fetch/news_fetcher.py
@@ -9,10 +9,10 @@ load_dotenv()
 NEWSAPI_KEY = os.getenv("NEWSAPI_KEY")
 
 # 스크래핑 해온 뉴스 기사를 가져오는 함수
-# query: 검색어, api_key: 뉴스 API 키, from_date: 시작 날짜,
+# query: 검색어, from_date: 시작 날짜,
 # to_date: 종료 날짜, num_articles: 가져올 기사 수
 # 각 기사의 title과 description을 포함한 리스트를 반환
-def get_latest_news(query, api_key, from_date, to_date, num_articles=5):
+def get_latest_news(query, from_date, to_date, num_articles=5):
     url = (
         f'https://newsapi.org/v2/everything'
         f'?q={query}'
@@ -20,7 +20,7 @@ def get_latest_news(query, api_key, from_date, to_date, num_articles=5):
         f'&to={to_date}'      
         f'&language=ko'       
         f'&sortBy=publishedAt'
-        f'&apiKey={api_key}'
+        f'&apiKey={NEWSAPI_KEY}'
     )
     response = requests.get(url)
     print(f"Fetching news for query: {query} from {from_date} to {to_date}")
@@ -29,7 +29,6 @@ def get_latest_news(query, api_key, from_date, to_date, num_articles=5):
         articles = response.json().get('articles', [])[:num_articles]
         result_list = [{'title': article['title'], 
                         'description': article['description'],}
-                        # 'url': article['url']} 
                         for article in articles]
     else:
         print(f"Error fetching news: {response.status_code}")
@@ -39,4 +38,4 @@ def get_latest_news(query, api_key, from_date, to_date, num_articles=5):
     return result_list
         
 # Example usage
-# print(get_latest_news('삼성', NEWSAPI_KEY, '2025-07-27', '2025-08-02'))
+# print(get_latest_news('삼성', '2025-07-27', '2025-08-02'))
